c788bc6e907c228e78c606e4c68d55503eb1576e
Luca Quatela <lucaqq@gmail.com>
2016-04-08 14:38:58 -0700

    confirm the user order and display a different message if the user insert an address

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 // businnessLogic START
 
-orderedPizzasArray = []; // thisArray contains the pizzas ordered by the user
+// orderedPizzasArray = []; // thisArray contains the pizzas ordered by the user
+
+var numberOfPizzas = 0;
+var userAddress = "";
 
 function pizzas (size, toppings, number){ // pizza constructor
   this.size = size;
@@ -30,9 +33,14 @@ pizzas.prototype.prizeDeterminer = function () {
 
 // user interface START
 
+// $("#size").click(function(){
+//   console.log("hello");
+// })
+
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
+    userAddress = $("#userAddress").val();
     pizzaSize = $("#size").val();
     var toppings = [];
     if ($("#topping1").val() != "none"){
@@ -47,18 +55,38 @@ $(document).ready(function(){
     if ($("#topping4").val() != "none"){
       toppings.push($("#topping4").val());
     }
-    var numberOfPizzas = orderedPizzasArray.length + 1;
+    // var numberOfPizzas = orderedPizzasArray.length + 1;
+    numberOfPizzas ++;
     var userPizza = new pizzas (pizzaSize, toppings, numberOfPizzas);
     userPizza.price = userPizza.prizeDeterminer();
-    orderedPizzasArray.push(userPizza);
-    $("#orderedPizzaDisplayer").append("<li><span id=" + userPizza.number + ">Pizza number " + userPizza.number + "</span></li>");
-    // $("#orderedPizzaDisplayer").text(orderedPizzasArray.forEach);
-    $("#" + userPizza.number + "").click(function(){
-      $("#sizeDetail").text(userPizza.size);
-      $("#toppingsDetail").text(userPizza.toppings);
-      $("#priceDetail").text(userPizza.price);
-    })
+    // orderedPizzasArray.push(userPizza);
+    $("#orderedPizzaDisplayer").append("<li><span id=" + userPizza.number + ">Pizza number " + userPizza.number + " (" + userPizza.size + ")</span></li>");
+    // $("#" + userPizza.number + "").click(function(){
+    $("#orderedPizzaDisplayer li").last().click(function(){
+      $("#sizeDetail").text("Size: " + userPizza.size);
+      if (userPizza.toppings != 0){
+      $("#toppingsDetail").text("Toppings: " + userPizza.toppings);
+      } else {
+        $("#toppingsDetail").empty(); // this line of code allow me to erase the "Toppings" section if no topping is selected for that pizza
+      }
+      $("#priceDetail").text("Price: " + userPizza.price);
+    });
+    $("#orderedPizza").show();
+    if (userAddress != "") {
+      $("#addressDisplayer").text("Your Pizza will be delivered at: " + userAddress);
+    }
   })
-})
+  $("#orderButton").click(function(){
+    $("#mainContainer").hide();
+    if (numberOfPizzas != 0) {
+      if (userAddress != ""){
+      $("#finalContainer").append("<p>Order registered, you will be delivered " + numberOfPizzas + " pizzas at " + userAddress + "</p>")
+    } else {
+      $("#finalContainer").append("<p>Order registered, your pizza will be ready in 30 minutes</p>")
+    }
+    }
+  });
+});
+
 
 // user interface END
